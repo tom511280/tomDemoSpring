8b9212e26b4f2fa73006af1b502128412e70ed1e
吳旻璁 <tom511280@gmail.com>
2019-08-29 22:07:09 +0800

    8/29加上ModelAndView範例與修改呈現

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tomDemoSpring
-實作一個spring4的範例專案
+實作一個spring4的範例專案  
+
+需加載log4j的配置文件才可於控制台打印相關log  
+
+
+## 使用ModelMap方式回應請求
+http://localhost:8080/TomDemoSpring/mainController/mainActionByModelMap
+
+## 使用ModelAndView方式回應請求
+http://localhost:8080/TomDemoSpring/mainController/mainActionByModelAndView
+
--- src/main/java/com/tomDemoSpring/controller/MainController.java
+++ src/main/java/com/tomDemoSpring/controller/MainController.java
@@ -1,23 +1,36 @@
 package com.tomDemoSpring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("mainController")
 public class MainController {
 	
 	/**
-	 * @see 範例入口
-	 * @param model
-	 * @return
+	 * @see 範例入口ByModelMap
+	 * @return String
 	 */
-	@RequestMapping(value="mainAction", method = RequestMethod.GET)
-	//@RequestMapping(method = RequestMethod.GET)
-    public String mainAction(ModelMap model) {
-        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+	@RequestMapping(value="mainActionByModelMap", method = RequestMethod.GET)
+    public String mainActionByModelMap(ModelMap model) {
+        model.addAttribute("responseType", "使用ModelMap方式回應請求");
         return "welcome";
+    }
+	
+	/**
+	 * @see 範例入口ByModelAndView
+	 * @return ModelAndView
+	 */
+	@RequestMapping(value="mainActionByModelAndView", method = RequestMethod.GET)
+    public ModelAndView mainActionByModelAndView() {
+		ModelAndView modelAndView = new ModelAndView("welcome");
+	    modelAndView.addObject("responseType", "使用ModelAndView方式回應請求");
+	    return modelAndView;
     }
 }
